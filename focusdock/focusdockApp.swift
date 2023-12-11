e5aaeac1eb9bb22199b2699e7425d257404c38db
Nils Uhrberg <nils.uhrberg@codecentric.de>
2023-12-11 23:35:55 +0100

    Initial Commit

---
+++ focusdock/focusdockApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusdockApp.swift
+//  focusdock
+//
+//  Created by Nils on 11.12.23.
+//
+
+import SwiftUI
+
+@main
+struct focusdockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
